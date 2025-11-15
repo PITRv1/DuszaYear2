4b525e76a614bb5cbda4e36befe809ea775d3997
PITRv1 <petersandor.toth@gmail.com>
2025-11-15 08:07:17 +0100

    asda

--- WizardGameProject/_Assets/Shaders/Compositor/pixelization_shader.gd
+++ WizardGameProject/_Assets/Shaders/Compositor/pixelization_shader.gd
@@ -59,6 +59,6 @@ func initialize_computer_shader() -> void:
 	rd = RenderingServer.get_rendering_device()
 	if not rd : return
 	
-	var glsl_file : RDShaderFile = load("res://assets/materials/shaders/compositor/pixelization_compute.glsl")
+	var glsl_file : RDShaderFile = load("res://_Assets/Shaders/Compositor/pixelization_compute.glsl")
 	shader = rd.shader_create_from_spirv(glsl_file.get_spirv())
 	pipeline = rd.compute_pipeline_create(shader)
