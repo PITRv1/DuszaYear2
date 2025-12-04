61a08fc7101bcfba94ed0c6ea4329249afdfd310
scynthi <85178488+scynthi@users.noreply.github.com>
2025-12-04 21:47:57 +0100

    backend

--- WizardGameProject/scripts/decks/pointcard_deck.gd
+++ WizardGameProject/scripts/decks/pointcard_deck.gd
@@ -4,7 +4,7 @@ class_name PointCardDeck extends DeckInterface
 @export_range(1, 9, 1) var cards_per_type : int = 3
 
 func generate_deck() -> void:
-	for point_card_num in range(1, card_types):
+	for point_card_num in range(1, card_types+1):
 		for per_type in range(0, cards_per_type):
 			var point_card := PointCard.new()
 			point_card.value = point_card_num
--- WizardGameProject/testing/test_scene.gd
+++ WizardGameProject/testing/test_scene.gd
@@ -3,6 +3,19 @@ extends Node
 
 func _ready() -> void:
 	var pointdeck := PointCardDeck.new()
+	var modifdeck := ModifierCardDeck.new()
+	
 	pointdeck.generate_deck()
+	modifdeck.generate_deck()
+	
+	var point_card : PointCard = pointdeck.draw_card()
+	print(point_card.value, " ", point_card.rarity)
+	
+	var modif_card : ModifierCardMultiplier = modifdeck.draw_card()
+	modif_card.randomize_properties()
+	print(modif_card.amount)
+	
+	point_card.add_modifier(modif_card)
+	print(point_card.value, " ", point_card.rarity)
 	
 	
