1da3a48fa131817a71dfcd24eb53a855213ccee9
donatmedve <spinodoo123@gmail.com>
2026-01-16 08:40:50 +0100

    theif woeked on

--- WizardGameProject/woryn/Scripts/Player/CThief.cs
+++ WizardGameProject/woryn/Scripts/Player/CThief.cs
@@ -4,15 +4,24 @@ using System;
 public class CThief : PlayerClassInterface
 {
 	public string ClassName { get; set; } = "Thief" ;
-    public int ActiveCooldown { get; set; } = 3 ;
+    public int ActiveCooldown { get; set; } = 32;
     public int PassiveCooldown { get; set; } = 0 ;
     public PlayerClass Parent { get; set; }
     public void UseActive()
     {
+        //steal points
+        //choose player here (UI)
+        PlayerClass choosenPlayer;
         
+        parent.points+= choosenPlayer.Points;
+        choosenPlayer.points = 0;
     }
     public void UsePassive()
     {
-        //még nincs passive countdown 
+        // Legyen megmutatva az első 3 káryta a pakliból a játékosnak és válasszon (UI)
+        PointCard choosenPCard;
+        ModifierCard choosenMCard;
+        
+        
     }
 }
