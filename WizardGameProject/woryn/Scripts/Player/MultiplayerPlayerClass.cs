dbd800605c074ab2899504f5b8ca8c11c7521991
PITRv1 <petersandor.toth@gmail.com>
2026-01-17 09:10:57 +0100

    last

--- WizardGameProject/woryn/Scripts/Player/MultiplayerPlayerClass.cs
+++ WizardGameProject/woryn/Scripts/Player/MultiplayerPlayerClass.cs
@@ -5,173 +5,173 @@ using System.Linq;
 
 public partial class MultiplayerPlayerClass : Node
 {
-    public PlayerClass playerClass;
-    public int ID;
-    // List<int> ids = new();
+	public PlayerClass playerClass;
+	public int ID;
+	// List<int> ids = new();
 
-    [Export] HBoxContainer pointCards;
-    [Export] HBoxContainer modifCards;
-    [Export] PackedScene pointCardUI;
-    [Export] PackedScene modifierCardUI;
-    [Export] Label maxPoints;
-    [Export] Label points;
-    [Export] Label throwDeckValue;
-    [Export] PackedScene shopScene;
-    
-    public override void _Ready()
-    {
-        ID = Global.multiplayerClientGlobals._id;
-        Global.multiplayerClientGlobals.HandleTurnInfo += playerClass.ProccessTurnInfoPacket;
-        Global.multiplayerClientGlobals.HandlePickUpCardAnswer += playerClass.ProccessPickUpAnswer;
-        Global.multiplayerClientGlobals.HandlePickUpCardAnswer += Burger;
-        Global.multiplayerClientGlobals.HandleDeckSwap += playerClass.HandleDeckSwap;
+	[Export] HBoxContainer pointCards;
+	[Export] HBoxContainer modifCards;
+	[Export] PackedScene pointCardUI;
+	[Export] PackedScene modifierCardUI;
+	[Export] Label maxPoints;
+	[Export] Label points;
+	[Export] Label throwDeckValue;
+	[Export] PackedScene shopScene;
+	
+	public override void _Ready()
+	{
+		ID = Global.multiplayerClientGlobals._id;
+		Global.multiplayerClientGlobals.HandleTurnInfo += playerClass.ProccessTurnInfoPacket;
+		Global.multiplayerClientGlobals.HandlePickUpCardAnswer += playerClass.ProccessPickUpAnswer;
+		Global.multiplayerClientGlobals.HandlePickUpCardAnswer += Burger;
+		Global.multiplayerClientGlobals.HandleDeckSwap += playerClass.HandleDeckSwap;
 
-        Global.multiplayerClientGlobals.ShopScene += GoToShop;
-    }
+		Global.multiplayerClientGlobals.ShopScene += GoToShop;
+	}
 
-    public void ClientReady()
-    {
-        ClientReady packet = new ClientReady();
+	public void ClientReady()
+	{
+		ClientReady packet = new ClientReady();
 
-        Global.networkHandler._serverPeer?.Send(0, packet.Encode(), (int)ENetPacketPeer.FlagReliable);
-    }
+		Global.networkHandler._serverPeer?.Send(0, packet.Encode(), (int)ENetPacketPeer.FlagReliable);
+	}
 
-    public MultiplayerPlayerClass()
-    {
-        playerClass = new PlayerClass();
-        playerClass.parent = this;
-    }
+	public MultiplayerPlayerClass()
+	{
+		playerClass = new PlayerClass();
+		playerClass.parent = this;
+	}
 
-    private void Local(int id)
-    {
-        ID = id;
-    }
+	private void Local(int id)
+	{
+		ID = id;
+	}
 
-    private void GoToShop()
-    {
+	private void GoToShop()
+	{
 		GetTree().ChangeSceneToPacked(shopScene);
-    }
+	}
 
-    // private void Remote(int id)
-    // {
-    //     ids.Add(id);
-    // }
+	// private void Remote(int id)
+	// {
+	//     ids.Add(id);
+	// }
 
-    private void Burger(byte[] data)
-    {
-        GD.Print("i hate");
-    }
+	private void Burger(byte[] data)
+	{
+		GD.Print("i hate");
+	}
 
-    public void SetUI(int mPoints, int plrPoints, int throwValue)
-    {
-        maxPoints.Text = mPoints.ToString();
-        points.Text = plrPoints.ToString();
-        throwDeckValue.Text = throwValue.ToString();
-    }
+	public void SetUI(int mPoints, int plrPoints, int throwValue)
+	{
+		maxPoints.Text = mPoints.ToString();
+		points.Text = plrPoints.ToString();
+		throwDeckValue.Text = throwValue.ToString();
+	}
 
-    public void PlayCard()
-    {
-        if (playerClass.CanEndTurn())
-            return;
+	public void PlayCard()
+	{
+		if (playerClass.CanEndTurn())
+			return;
 
-        List<byte> modifIndexes = new List<byte>();
+		List<byte> modifIndexes = new List<byte>();
 
-        foreach (ModifierCard card in playerClass.chosenModifierCards)
-        {
-            modifIndexes.Add((byte)playerClass.ModifCardList.IndexOf(card));
-        }
+		foreach (ModifierCard card in playerClass.chosenModifierCards)
+		{
+			modifIndexes.Add((byte)playerClass.ModifCardList.IndexOf(card));
+		}
 
-        EndTurnRequest packet = new EndTurnRequest
-        {
-            SenderId = ID,
-            PointCard = playerClass.chosenPointCard,
-            PointCardIndex = playerClass.PointCardList.IndexOf(playerClass.chosenPointCard),
-            ModifierCards = playerClass.chosenModifierCards.ToArray(),
-            ModifCardIndexes = modifIndexes.ToArray()
-        };
+		EndTurnRequest packet = new EndTurnRequest
+		{
+			SenderId = ID,
+			PointCard = playerClass.chosenPointCard,
+			PointCardIndex = playerClass.PointCardList.IndexOf(playerClass.chosenPointCard),
+			ModifierCards = playerClass.chosenModifierCards.ToArray(),
+			ModifCardIndexes = modifIndexes.ToArray()
+		};
 
-        Global.networkHandler._serverPeer?.Send(0, packet.Encode(), (int)ENetPacketPeer.FlagReliable);
-    }
+		Global.networkHandler._serverPeer?.Send(0, packet.Encode(), (int)ENetPacketPeer.FlagReliable);
+	}
 
-    public void RemoveSelectedCards(int lastPlayer)
-    {
-        if (lastPlayer != ID)
-            return;
-        pointCards.RemoveChild(pointCards.GetChild(playerClass.PointCardList.IndexOf(playerClass.chosenPointCard)));
-        playerClass.PointCardList.Remove(playerClass.chosenPointCard);
+	public void RemoveSelectedCards(int lastPlayer)
+	{
+		if (lastPlayer != ID)
+			return;
+		pointCards.RemoveChild(pointCards.GetChild(playerClass.PointCardList.IndexOf(playerClass.chosenPointCard)));
+		playerClass.PointCardList.Remove(playerClass.chosenPointCard);
 
-        List<int> modifIndexes = new List<int>();
+		List<int> modifIndexes = new List<int>();
 
-        foreach (ModifierCard card in playerClass.chosenModifierCards)
-        {
-            modifIndexes.Add(playerClass.ModifCardList.IndexOf(card));
-            playerClass.ModifCardList.Remove(card);
-        }
+		foreach (ModifierCard card in playerClass.chosenModifierCards)
+		{
+			modifIndexes.Add(playerClass.ModifCardList.IndexOf(card));
+			playerClass.ModifCardList.Remove(card);
+		}
 
-        modifIndexes.Sort();
-        modifIndexes.Reverse();
+		modifIndexes.Sort();
+		modifIndexes.Reverse();
 
-        foreach (int index in modifIndexes)
-        {
-            modifCards.RemoveChild(modifCards.GetChild(index));
-        }
+		foreach (int index in modifIndexes)
+		{
+			modifCards.RemoveChild(modifCards.GetChild(index));
+		}
 
-        playerClass.chosenModifierCards.Clear();
-        playerClass.chosenPointCard = null;
-    }
+		playerClass.chosenModifierCards.Clear();
+		playerClass.chosenPointCard = null;
+	}
 
-    public void PickUpCards()
-    {
-        PickUpCardRequest packet = new PickUpCardRequest
-        {
-            SenderId = ID,
-        };
+	public void PickUpCards()
+	{
+		PickUpCardRequest packet = new PickUpCardRequest
+		{
+			SenderId = ID,
+		};
 
-        Global.networkHandler._serverPeer?.Send(0, packet.Encode(), (int)ENetPacketPeer.FlagReliable);
-    }
+		Global.networkHandler._serverPeer?.Send(0, packet.Encode(), (int)ENetPacketPeer.FlagReliable);
+	}
 
-    public void PlayAbilityRequest()
-    {
-        
-    }
+	public void PlayAbilityRequest()
+	{
+		
+	}
 
-    public void AddPointToContainer(PointCard pointCard)
-    {
-        TestPointCardUi test = pointCardUI.Instantiate() as TestPointCardUi;
-        test.text.Text = pointCard.PointValue.ToString();
-        test.pointCard = pointCard;
-        test.playerClass = playerClass;
-        pointCards.AddChild(test);
-    }
+	public void AddPointToContainer(PointCard pointCard)
+	{
+		TestPointCardUi test = pointCardUI.Instantiate() as TestPointCardUi;
+		test.text.Text = pointCard.PointValue.ToString();
+		test.pointCard = pointCard;
+		test.playerClass = playerClass;
+		pointCards.AddChild(test);
+	}
 
-    public void AddModifierToContainer(ModifierCard card)
-    {
-        TestModifierCardUi test = modifierCardUI.Instantiate() as TestModifierCardUi;
+	public void AddModifierToContainer(ModifierCard card)
+	{
+		TestModifierCardUi test = modifierCardUI.Instantiate() as TestModifierCardUi;
 
-        switch (card.ModifierType)
-        {
-            case MODIFIER_TYPES.MULTIPLIER:
-                test.text.Text = $"[wave freq=1][rainbow freq=0.1]*{(card as ModifierCardMultiplier).Amount}[/rainbow][/wave]";
-                test.modifierCard = card;
-                test.playerClass = playerClass;
-                break;
-            case MODIFIER_TYPES.ADDITION:
-                test.text.Text = $"[wave freq=1][rainbow freq=0.1]+{(card as ModifierCardAddition).Amount}[/rainbow][/wave]";
-                test.modifierCard = card;
-                test.playerClass = playerClass;
-                break;
-            case MODIFIER_TYPES.SKIP:
-                test.text.Text = "[wave freq=1][rainbow freq=0.1]Skip[/rainbow][/wave]";
-                test.modifierCard = card;
-                test.playerClass = playerClass;
-                break;
-            case MODIFIER_TYPES.REVERSE:
-                test.text.Text = "[wave freq=1][rainbow freq=0.1]Reverse[/rainbow][/wave]";
-                test.modifierCard = card;
-                test.playerClass = playerClass;
-                break;
-        }
+		switch (card.ModifierType)
+		{
+			case MODIFIER_TYPES.MULTIPLIER:
+				test.text.Text = $"[wave freq=1][rainbow freq=0.1]*{(card as ModifierCardMultiplier).Amount}[/rainbow][/wave]";
+				test.modifierCard = card;
+				test.playerClass = playerClass;
+				break;
+			case MODIFIER_TYPES.ADDITION:
+				test.text.Text = $"[wave freq=1][rainbow freq=0.1]+{(card as ModifierCardAddition).Amount}[/rainbow][/wave]";
+				test.modifierCard = card;
+				test.playerClass = playerClass;
+				break;
+			case MODIFIER_TYPES.SKIP:
+				test.text.Text = "[wave freq=1][rainbow freq=0.1]Skip[/rainbow][/wave]";
+				test.modifierCard = card;
+				test.playerClass = playerClass;
+				break;
+			case MODIFIER_TYPES.REVERSE:
+				test.text.Text = "[wave freq=1][rainbow freq=0.1]Reverse[/rainbow][/wave]";
+				test.modifierCard = card;
+				test.playerClass = playerClass;
+				break;
+		}
 
-        modifCards.AddChild(test);
-    }
+		modifCards.AddChild(test);
+	}
 }
